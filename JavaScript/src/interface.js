a31e6784d5b0454e7d3d13a3acdcf750aa516ba1
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-23 17:27:10 +0000

    Display all tasks

--- JavaScript/src/interface.js
+++ JavaScript/src/interface.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
 
-  function displayTasks(){
+  function getTasks(){
     var url = 'http://localhost:9292/api/todo';
     $.get(url, function(data){
-      console.log(data)
-      $('#tasks').text(data.task);
+      // $('#tasks').text(data.task);
+      displayTasks(data.task)
     })
   }
 
+  function displayTasks(tasks) {
+    var str = ""
+    for(var i = 0; i < tasks.length; i++) {
+        str += "<li>" + tasks[i].content + "</li>";
+    };
+    $('#todo').html(str);
+  };
+
   $('#getTasks').click(function(){
-    displayTasks();
-
-
+    getTasks();
   });
 });
